18ba554e03d21f0ef5bea5cb75f61ee7baad3095
Raghul <raghsiva@outlook.com>
2019-07-29 21:48:31 +0530

    Initial user details

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import GitHubSearch from './Component/GithubSearch';
+
+ReactDOM.render(
+    <GitHubSearch />,
+    document.querySelector("#root")
+);
